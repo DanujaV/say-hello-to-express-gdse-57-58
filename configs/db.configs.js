5d038d0354925eb69741580822ff4883f68393bd
danu <danujagreru@gmail.com>
2022-08-16 09:42:26 +0530

    create connection with MySQL Server

---
+++ configs/db.configs.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'Danu25412541@',
+        database: 'express'
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const mysql = require('mysql')
+const db = require('../configs/db.configs')
+
+const connection = mysql.createConnection(db.database)
+
+connection.connect(function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the MySQL server');
+    }
+})
 
 const router = express.Router()
 
@@ -7,7 +19,21 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+    console.log(req.body);
     res.send('user post method')
+})
+
+router.put('/', (req, res) => {
+    res.send('put method')
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('delete method')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('get user by id')
 })
 
 //put
